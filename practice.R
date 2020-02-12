685aa26ce6bef8f801803bdc49333d965db80865
mlu13 <mlu13@usfca.edu>
2020-02-11 21:18:35 -0800

    new file

--- practice.R
+++ practice.R
@@ -55,28 +55,3 @@ class(x)
 #C=5/9×( F −32)
 city_temp$temp <- (5/9)*(city_temp$temp-32)
 
-a <- c(4,5,6)
-b <- c(7,8,9)
-dist(rbind(a, b))
-
-oddnum <- function(n){
-  if(isTRUE(n%%2 != 0)){
-    print("True")
-  }
-  else{
-    print("False")
-  }
-}
-
-oddnum(15)
-
-x <- c(6,7,8,9)
-for(e in x){print(e)}
-
-i = 0
-while (i < 10){
-  print(i)
-  i = i - 1
-}
-
-
